651268447422902aca0d581f613f356ec6e45a41
Ruiz Molina Fernanda <168505783+Fer-Ruiz-Molina@users.noreply.github.com>
2024-06-03 17:55:45 -0600

    Update 11.cpp

--- 11.cpp
+++ 11.cpp
@@ -1,17 +1,17 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int main(){
+
+int main() {
     int num[5], numero;
-    for (int i =0; i < 5; i++)
-    {
-        cout<<"digite un numero para la posicion",i;
-        cin>>numero;
-        num[1]<-numero;   
+    for (int i = 0; i < 5; i++) {
+        cout << "Digite un número para la posición " << i << ": ";
+        cin >> numero;
+        num[i] = numero;
     }
-    for (int i = 0; i < 5 ; i ++)
-    {
-        cout<<"el dato de la posicion"<<i+1<<"es: "<<"\n";
+
+    for (int i = 0; i < 5; i++) {
+        cout << "El dato de la posición " << i + 1 << " es: " << num[i] << "\n";
     }
+
     return 0;
-    
 }
